562004f8b3914ba02c081871eade33499241bf2e
Oathbound <latiosgigetto@gmail.com>
2024-04-30 00:05:00 +0200

    Many fixes

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import logging
-import concurrent.futures
+from multiprocessing import Pool, Manager
+import os
 
 import requests
 from bs4 import BeautifulSoup
@@ -7,11 +8,18 @@ import pandas as pd
 import PyPDF2
 
 
-# Make an excel document containing the URL where the word "blockchain" was found
-# and a field saing whether the word was found or not or if there was an error
-list = []
+# Set up the logger
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename='azioni.log', level=logging.INFO)
+logger.setLevel(logging.INFO)
+
+handler = logging.FileHandler('azioni.log')
+handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('PID: %(process)d - %(asctime)s - %(message)s')
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
 # Read the excel file
 data = pd.read_excel('input.xlsx')
 logger.info('The input file was read successfully.')
@@ -29,13 +37,18 @@ def scrape_url(url):
         response = requests.get(url)
     except requests.exceptions.RequestException as e:
         logger.error('There was an error while scraping the following link: ' + url)
-        list.append({url, 'Errore', '-', '-'})
+        shared_list.append((url, 'Errore', '-', '-'))
         return e
     soup = BeautifulSoup(response.text, 'html.parser')
-    body = soup.find('body').get_text()
+    try:
+        body = soup.find('body').get_text()
+    except AttributeError as e:
+        logger.error('There was an error while scraping the following link: ' + url)
+        shared_list.append((url, 'Errore', '-', '-'))
+        return
     if 'blockchain' in body:
         logger.info('The word "blockchain" was found in the homepage:' + url)
-        list.append({url, 'Sì', '-', '-'})
+        shared_list.append((url, 'Sì', '-', '-'))
         return True
     else:
         scrape_related_links(url, response)
@@ -53,7 +66,7 @@ def scrape_related_links(url, sourceResponse):
                 response = requests.get(href)
             except requests.exceptions.RequestException as e:
                 logger.error('There was an error while scraping the following link: ' + href)
-                list.append({url, 'No', 'Errore', href})
+                shared_list.append((url, 'No', 'Errore', href))
                 return e
             if response.headers['Content-Type'] == 'application/pdf':
                 scrape_pdf(response.content, url, href)
@@ -62,13 +75,14 @@ def scrape_related_links(url, sourceResponse):
                 body = soup.find('body').get_text()
                 if 'blockchain' in body:
                     logger.info('The word "blockchain" was found in the related link:' + href)
-                    list.append({url, 'No', 'Sì', href})
+                    shared_list.append((url, 'No', 'Sì', href))
                     return True
                 else:
                     logger.info('The word "blockchain" was not found in the related link:' + href)
                     continue
     logger.info('The word "blockchain" was not found in any related links.')
-    list.append(url, 'No', 'No', '-')
+    shared_list.append((url, 'No', 'No', '-'))
+    print('List length:', len(shared_list))
     return False
 
 
@@ -80,7 +94,7 @@ def scrape_pdf(pdf, homepage_url, url):
         text += page.extract_text()
     if 'blockchain' in text:
         logger.info('The word "blockchain" was found in the related link:' + url)
-        list.append({homepage_url, 'No', 'Sì', url})
+        shared_list.append((homepage_url, 'No', 'Sì', url))
         return
     else:
         logger.info('The word "blockchain" was not found in the related link:' + url)
@@ -88,16 +102,27 @@ def scrape_pdf(pdf, homepage_url, url):
 
 
 
-def process_row(row):
+def process_row(args):
+    index, row, list = args
     homepage_url = row['Website']
     logger.info('Scraping the homepage: ' + homepage_url)
+    if homepage_url.startswith('http') is False:
+        homepage_url = 'http://' + homepage_url
     scrape_url(homepage_url)
 
-# Create a ThreadPoolExecutor
-with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-    # Map the function to the data
-    executor.map(lambda row: process_row(row[1]), data.iterrows())  
+# Create a Manager
+with Manager() as manager:
+    # Create a list in shared memory
+    shared_list = manager.list()
+
+    # Create a Pool
+    with Pool() as pool:
+        # Map the function to the data
+                pool.map(process_row, [(index, row, list) for index, row in data.iterrows()])
+    final_list = list(shared_list)
 
 # Save the output to an excel file
-output = pd.DataFrame(list, columns = ['Webpage', 'Trovata in Home', 'Trovata in link', 'Link' ])
+logger.info('Saving the output to an excel file.')
+output = pd.DataFrame(final_list, columns = ['Webpage', 'Trovata in Home', 'Trovata in link', 'Link' ])
 output.to_excel('output.xlsx')
+logger.info('The output was saved successfully.')
