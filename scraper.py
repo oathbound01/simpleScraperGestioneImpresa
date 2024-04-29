4561e66922045b2af87be21ff14b56751f378df5
Oathbound <latiosgigetto@gmail.com>
2024-04-29 17:59:38 +0200

    Fixes

--- scraper.py
+++ scraper.py
@@ -9,11 +9,12 @@ import PyPDF2
 
 # Make an excel document containing the URL where the word "blockchain" was found
 # and a field saing whether the word was found or not or if there was an error
-output = pd.DataFrame(list, columns = ['Webpage', 'Trovata in Home', 'Trovata in link', 'Link' ])
+list = []
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='azioni.log', level=logging.INFO)
 # Read the excel file
 data = pd.read_excel('input.xlsx')
+logger.info('The input file was read successfully.')
 
 # Check the homepage for URLs that are of the same domain
 def is_same_domain(url, domain):
@@ -28,13 +29,13 @@ def scrape_url(url):
         response = requests.get(url)
     except requests.exceptions.RequestException as e:
         logger.error('There was an error while scraping the following link: ' + url)
-        output.append(url, 'Errore', '-', '-')
+        list.append({url, 'Errore', '-', '-'})
         return e
     soup = BeautifulSoup(response.text, 'html.parser')
     body = soup.find('body').get_text()
     if 'blockchain' in body:
         logger.info('The word "blockchain" was found in the homepage:' + url)
-        output.append(url, 'Sì', '-', '-')
+        list.append({url, 'Sì', '-', '-'})
         return True
     else:
         scrape_related_links(url, response)
@@ -52,7 +53,7 @@ def scrape_related_links(url, sourceResponse):
                 response = requests.get(href)
             except requests.exceptions.RequestException as e:
                 logger.error('There was an error while scraping the following link: ' + href)
-                output.append(url, 'No', 'Errore', href)
+                list.append({url, 'No', 'Errore', href})
                 return e
             if response.headers['Content-Type'] == 'application/pdf':
                 scrape_pdf(response.content, url, href)
@@ -61,13 +62,13 @@ def scrape_related_links(url, sourceResponse):
                 body = soup.find('body').get_text()
                 if 'blockchain' in body:
                     logger.info('The word "blockchain" was found in the related link:' + href)
-                    output.append(url, 'No', 'Sì', href)
+                    list.append({url, 'No', 'Sì', href})
                     return True
                 else:
                     logger.info('The word "blockchain" was not found in the related link:' + href)
                     continue
     logger.info('The word "blockchain" was not found in any related links.')
-    output.append(url, 'No', 'No', '-')
+    list.append(url, 'No', 'No', '-')
     return False
 
 
@@ -79,7 +80,7 @@ def scrape_pdf(pdf, homepage_url, url):
         text += page.extract_text()
     if 'blockchain' in text:
         logger.info('The word "blockchain" was found in the related link:' + url)
-        output.append(homepage_url, 'No', 'Sì', url)
+        list.append({homepage_url, 'No', 'Sì', url})
         return
     else:
         logger.info('The word "blockchain" was not found in the related link:' + url)
@@ -88,14 +89,15 @@ def scrape_pdf(pdf, homepage_url, url):
 
 
 def process_row(row):
-    homepage_url = data['Website'][0]
-    domain = homepage_url.split('//')[1].split('/')[0]
+    homepage_url = row['Website']
+    logger.info('Scraping the homepage: ' + homepage_url)
     scrape_url(homepage_url)
 
 # Create a ThreadPoolExecutor
-with concurrent.futures.ThreadPoolExecutor() as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
     # Map the function to the data
-    executor.map(process_row, data.iterrows())
+    executor.map(lambda row: process_row(row[1]), data.iterrows())  
 
 # Save the output to an excel file
+output = pd.DataFrame(list, columns = ['Webpage', 'Trovata in Home', 'Trovata in link', 'Link' ])
 output.to_excel('output.xlsx')
