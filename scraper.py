d5c15264fcf87d78192f36684dfe45187bd900aa
FrancescoParisi02 <francescoparisi178@gmail.com>
2024-04-30 12:01:54 +0200

    change .log file

--- scraper.py
+++ scraper.py
@@ -8,6 +8,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import PyPDF2
+import time
 
 
 # Check the homepage for URLs that are of the same domain
@@ -125,7 +126,8 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
 
-    handler = logging.FileHandler('azioni.log')
+    log_filename = 'azioni_' + time.strftime('%Y%m%d%H%M%S') + '.log'
+    handler = logging.FileHandler(log_filename)
     handler.setLevel(logging.INFO)
 
     formatter = logging.Formatter('PID:  %(process)s - %(asctime)s - %(message)s')
@@ -166,3 +168,4 @@ if __name__ == '__main__':
     output = pd.DataFrame(final_list, columns = ['Webpage', 'Trovata in Home', 'Trovata in link', 'Link' ])
     output.to_excel('output.xlsx')
     logger.info('The output was saved successfully.')
+    
